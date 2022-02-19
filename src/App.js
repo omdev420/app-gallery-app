e9f6f1109d03ee88a6c82ed20afedf0b0b2f6d13
omdev420 <omdev420@gmail.com>
2022-02-19 16:38:35 +0530

    connect to phantom wallet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import instagramLogo from "./assets/instagram-logo.svg";
 import artGalleryLogo from "./assets/art-gallery-logo.svg";
 import "./App.css";
@@ -8,9 +8,60 @@ const INSTAGRAM_HANDLE = "shubhankardev";
 const INSTAGRAM_LINK = `https://instagram.com/${INSTAGRAM_HANDLE}`;
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom Wallet found!");
+
+          const response = await solana.connect({ onlyIfTrusted: true });
+
+          console.log(
+            "Connected with Public Key:",
+            response.publicKey.toString(),
+          );
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        alert("Solana object not found! Get a Phantom Wallet 👻");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window;
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with Public Key:", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? "authed-container" : "container"}>
         <div className="header-container">
           <img
             alt="Art Gallery Icon"
@@ -21,6 +72,7 @@ const App = () => {
           <p className="sub-text">
             Showcase your art collection in the metaverse ✨
           </p>
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img
